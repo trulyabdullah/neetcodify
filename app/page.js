6310181364a6a8e0e231fa1056992ce98b9c3f6f
Abdullah Ansari <120268836+trulyabdullah@users.noreply.github.com>
2026-09-22 17:28:40 +0530

    clerk working

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import { onBoardUser } from "@/modules/auth/actions";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+	await onBoardUser();
 	return (
 		<div className="flex min-h-screen justify-center items-center">
 			<UserButton />
---
+++ modules/auth/actions/index.js
@@ -0,0 +1,40 @@
+"use server";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onBoardUser = async () => {
+	try {
+		const user = await currentUser();
+		if (!user)
+			return { success: false, error: "No authenticated user found" };
+		const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+
+		const newUser = await db.user.upsert({
+			where: {
+				clerkId: id,
+			},
+			update: {
+				firstName: firstName || null,
+				lastName: lastName || null,
+				imageUrl: imageUrl || null,
+				email: emailAddresses[0]?.emailAddress || "",
+			},
+			create: {
+				clerkId: id,
+				firstName: firstName || null,
+				lastName: lastName || null,
+				imageUrl: imageUrl || null,
+				email: emailAddresses[0]?.emailAddress || "",
+			},
+		});
+
+		return {
+			success: true,
+			user: newUser,
+			message: "User on boarded successfully.",
+		};
+	} catch (error) {
+		console.error("Error onboarding user.: ", error);
+		return { success: false, error: "Failed to on board user." };
+	}
+};
